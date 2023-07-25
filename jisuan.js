9238a1aa9a7ba5439328114b41ae441de7394511
‘dingyl666’ <1811239843@qq.com>
2023-07-25 11:17:27 +0800

    多层for

--- jisuan.js
+++ jisuan.js
@@ -22,7 +22,7 @@ const arr = [
     [7,8,9],
     [1,2,3],
 ]
-//
+//多层for循环 记住 外层跑一次 内层跑全部
 const handleTotalRow = (array) => {
     const newArray = [];
     array.forEach(sub => {
@@ -40,7 +40,7 @@ const handleTotalRow = (array) => {
 
     return newArray ;
 }
-const data = handleTotalRow(arr) ;
+// const data = handleTotalRow(arr) ;
 console.log()
 // let index = 0 ;
 // function add(current = arr[0]) {
@@ -57,3 +57,39 @@ console.log()
 // add()
 
 // console.log(add())
+
+const threeArr = [
+    [
+        [1,2,3],
+        [4,5,6],
+    ],
+    [
+        [7,8,9],
+        [10,11,12],
+    ]
+]
+
+const handleThree = (array) => {
+    const newArray = [];
+    array.forEach(sub => {
+        console.log('第一层:', sub)
+        sub.forEach((num, index) => {
+            console.log('第二层：', num )
+            num.forEach(ff => {
+
+                // if(newArray[index]){
+                //     newArray[index] += ff;
+                // }else{
+                //     newArray[index] = ff;
+                // }
+                console.log('第三层：', num ,ff)
+            })
+        
+        });
+    });
+
+    return newArray ;
+}
+
+const bb = handleThree(threeArr)
+
